572c88ee09b1128f5ffc1b29147fd244ee9efc68
ZZZZZZZZ <574802621@qq.com>
2020-07-03 23:36:34 +0800

    Update 1480_1486_🗡指58_0203_1601.cpp

--- Leecode/1480_1486_🗡指58_0203_1601.cpp
+++ Leecode/1480_1486_🗡指58_0203_1601.cpp
@@ -1,34 +1,4 @@
-Question1480:
-    vector<int> runningSum(vector<int>& nums) {
-        int sum=0;
-        for(int i=0;i<nums.size();++i){
-            sum+=nums[i];
-            nums[i]=sum;
-        }
-        return nums;
-    }
-
-Question1486:
-    int xorOperation(int n, int start) {
-        vector<int> testVec;
-        for(int i=0;i<n;++i){
-            testVec.push_back(start+2*i);
-        }
-        int res=testVec[0];
-        for(int i=1;i<testVec.size();++i){
-            res=res^(testVec[i]);
-        }
-        return res;
-    }
-
-Question🗡指58:
-    string reverseLeftWords(string s, int n) {
-        string temps=s.substr(0,n);
-        s=s.substr(n,s.size()-1);
-        s=s+temps;
-        return s;
-    }
-
+//五道简单题这两道还有点意思
 面试题02.03:
     void deleteNode(ListNode* node) {
         ListNode* nextNode=node->next->next;
